e0857efebeb6c30ead8fc3a2f40f584e2dc074b4
Bidzhou <89260226211@mail.ru>
2024-09-15 05:51:18 +0300

    home page looks bomboclat good type shi

---
+++ Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by Frederico del' Bidzho on 15.09.2024.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+}
