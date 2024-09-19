862e3ce94635d12a8b5a9fdd6e5f1290e4146996
Bidzhou <89260226211@mail.ru>
2024-09-19 14:51:12 +0300

    done

---
+++ Netflix Clone/Managers/DataPersistenceManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataPersistenceManager.swift
+//  Netflix Clone
+//
+//  Created by Frederico del' Bidzho on 18.09.2024.
+//
+
+import Foundation
---
+++ Netflix Clone/Models/YouTubeSearchResponse.swift
@@ -0,0 +1,8 @@
+//
+//  YouTubeSearchResponse.swift
+//  Netflix Clone
+//
+//  Created by Frederico del' Bidzho on 16.09.2024.
+//
+
+import Foundation
